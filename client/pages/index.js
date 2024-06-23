d30876a785d3f0791c05ef6db8d9d51768f77a2c
Clement Yanze Lu <lucaslyz03@gmail.com>
2024-06-23 15:49:40 +0800

    setup

---
+++ client/pages/index.js
@@ -0,0 +1,10 @@
+let Test = () => {
+    return (
+        <>
+        <h1 className="jumbotron">World Metro Web</h1>
+        <h3>First independent project, travel transportation guide & info for multiple countries. </h3>
+        </>
+    );
+};
+
+export default Test;
